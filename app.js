c6406035595a1ea19bf543249ee6d575913cedec
Elizabeth Turner <99226082+elizaturnercodes@users.noreply.github.com>
2024-12-29 14:45:05 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // CORS configuration
 app.use(
   cors({
-    origin: ['https://storeroomclient.vercel.app', 'https://storeroomamu.netlify.app/', 'https://storeroomserver.vercel.app'],
+    origin: ['https://storeroomclient.vercel.app', 'https://storeroomserver.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
