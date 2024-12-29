db09642cef7513f86a8aaa658ac3f005538740c3
Elizabeth Turner <99226082+elizaturnercodes@users.noreply.github.com>
2024-12-29 00:39:59 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -13,14 +13,12 @@ const db = new sqlite3.Database(dbPath);
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(
-//   cors({
-//     origin: 'https://storeroomamu.netlify.app',
-//     credentials: true,
-//   })
-// );
-
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://storeroomamu.netlify.app',
+    credentials: true,
+  })
+);
 
 db.serialize(() => {
   db.run(
@@ -44,25 +42,9 @@ db.serialize(() => {
   });
 });
 
-db.run(
-  'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT UNIQUE, password TEXT, firstName TEXT, lastName TEXT, role TEXT CHECK(role IN ("admin", "employee")))',
-  (err) => {
-    if (err) {
-      console.error('Error creating users table', err.message);
-    } else {
-      console.log('Users table created or already exists');
-    }
-  }
-);
-
-app.use((req, res, next) => {
-  req.db = db;
-  next();
-});
-
 app.use('/api', routes);
 
-const userRoutes = require('./routes/userRoutes');
-app.use('/users', userRoutes);
-
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
